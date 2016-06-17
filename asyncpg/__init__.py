cc62a1eea746a7191b4a07a48dcf55f4c76787ee
Yury Selivanov <yury@magic.io>
2016-06-17 17:45:35 -0400

    Use loop.create_future if it exists

--- asyncpg/__init__.py
+++ asyncpg/__init__.py
@@ -16,12 +16,12 @@ class Connection:
         return self._protocol.get_settings()
 
     async def query(self, query):
-        waiter = asyncio.Future(loop=self._loop)
+        waiter = _create_future(self._loop)
         self._protocol.query(query, waiter)
         return await waiter
 
     async def prepare(self, query):
-        waiter = asyncio.Future(loop=self._loop)
+        waiter = _create_future(self._loop)
         self._protocol.prepare(None, query, waiter)
         state = await waiter
         return PreparedStatement(self, state)
@@ -34,7 +34,7 @@ class PreparedStatement:
 
     async def execute(self, *args):
         protocol = self._connection._protocol
-        waiter = asyncio.Future(loop=self._connection._loop)
+        waiter = _create_future(self._connection._loop)
         protocol.execute(self._state, args, waiter)
         return await waiter
 
@@ -54,3 +54,12 @@ async def connect(host='localhost', port=5432, user='postgres', *,
     await connected
 
     return Connection(pr, tr, loop)
+
+
+def _create_future(loop):
+    try:
+        create_future = loop.create_future
+    except AttributeError:
+        return asyncio.Future(loop=loop)
+    else:
+        return create_future()
