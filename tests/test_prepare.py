6c4883d6e4e65c9d6618244d821ca44c59ca5d58
Yury Selivanov <yury@magic.io>
2016-06-29 16:27:56 -0400

    tests: Test that we handle None->NULL conversion for TEXT and BINARY

--- tests/test_prepare.py
+++ tests/test_prepare.py
@@ -18,9 +18,29 @@ class TestPrepare(tb.ConnectedTestCase):
             await self.con.prepare('SELECT a')
 
     async def test_prepare_3(self):
-        st = await self.con.prepare('''
-            SELECT CASE WHEN $1::text IS NULL THEN 'NULL'
-                                              ELSE $1::text END''')
+        cases = [
+            ('text', ("'NULL'", 'NULL'), [
+                'aaa',
+                None
+            ]),
 
-        self.assertEqual((await st.execute('aaa'))[0][0], 'aaa')
-        self.assertEqual((await st.execute(None))[0][0], 'NULL')
+            ('decimal', ('0', 0), [
+                123,
+                123.5,
+                None
+            ])
+        ]
+
+        for type, (none_name, none_val), vals in cases:
+            st = await self.con.prepare('''
+                    SELECT CASE WHEN $1::{type} IS NULL THEN {default}
+                    ELSE $1::{type} END'''.format(
+                type=type, default=none_name))
+
+            for val in vals:
+                with self.subTest(type=type, value=val):
+                    res = (await st.execute(val))[0][0]
+                    if val is None:
+                        self.assertEqual(res, none_val)
+                    else:
+                        self.assertEqual(res, val)
