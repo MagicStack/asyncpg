12d855f287c27f8c3a71127bcd9269c6514768d2
Yury Selivanov <yury@magic.io>
2016-06-30 17:19:49 -0400

    Factor out Transaction class in a separate module

--- asyncpg/__init__.py
+++ asyncpg/__init__.py
@@ -1,11 +1,11 @@
 import asyncio
-import enum
 import getpass
 import os
 
 
 from . import introspection as _intro
 from .exceptions import *
+from .transaction import Transaction
 from .protocol import Protocol
 
 
@@ -116,133 +116,6 @@ class PreparedStatement:
     async def execute(self, *args):
         protocol = self._connection._protocol
         return await protocol.execute(self._state, args)
-
-
-class TransactionState(enum.Enum):
-    NEW             = 0
-    STARTED         = 1
-    COMMITTED       = 2
-    ROLLEDBACK      = 3
-    FAILED          = 4
-
-
-class Transaction:
-
-    ISOLATION_LEVELS = {'read_committed', 'serializable', 'repeatable_read'}
-
-    __slots__ = ('_connection', '_isolation', '_readonly', '_deferrable',
-                 '_state', '_nested', '_id')
-
-    def __init__(self, connection, isolation, readonly, deferrable):
-        if isolation not in self.ISOLATION_LEVELS:
-            raise ValueError(
-                'isolation is expected to be either of {}, '
-                'got {!r}'.format(self.ISOLATION_LEVELS, isolation))
-
-        if isolation != 'serializable':
-            if readonly:
-                raise ValueError(
-                    '"readonly" is only supported for '
-                    'serializable transactions')
-
-            if deferrable and not readonly:
-                raise ValueError(
-                    '"deferrable" is only supported for '
-                    'serializable readonly transactions')
-
-        self._connection = connection
-        self._isolation = isolation
-        self._readonly = readonly
-        self._deferrable = deferrable
-        self._state = TransactionState.NEW
-        self._nested = False
-        self._id = None
-
-    async def __aenter__(self):
-        await self.start()
-
-    async def __aexit__(self, extype, ex, tb):
-        if extype is not None:
-            await self.rollback()
-
-    async def start(self):
-        if self._state is not TransactionState.NEW:
-            raise FatalError('cannot start transaction: inconsistent state')
-
-        con = self._connection
-
-        if con._top_xact is None:
-            con._top_xact = self
-        else:
-            # Nested transaction block
-            top_xact = con._top_xact
-            if self._isolation != top_xact._isolation:
-                raise FatalError(
-                    'nested transaction has different isolation level: '
-                    'current {!r} != outer {!r}'.format(
-                        self._isolation, top_xact._isolation))
-            self._nested = True
-
-        if self._nested:
-            self._id = con._get_unique_id()
-            query = 'SAVEPOINT {};'.format(self._id)
-        else:
-            if self._isolation == 'read_committed':
-                query = 'BEGIN;'
-            elif self._isolation == 'repeatable_read':
-                query = 'BEGIN ISOLATION LEVEL REPEATABLE READ;'
-            else:
-                query = 'BEGIN ISOLATION LEVEL SERIALIZABLE'
-                if self._readonly:
-                    query += ' READ ONLY'
-                if self._deferrable:
-                    query += ' DEFERRABLE'
-                query += ';'
-
-        try:
-            await self._connection.execute_script(query)
-        except:
-            self._state = TransactionState.FAILED
-            raise
-        else:
-            self._state = TransactionState.STARTED
-
-    async def commit(self):
-        if self._state is not TransactionState.STARTED:
-            raise FatalError('cannot commit transaction: inconsistent state')
-
-        if self._nested:
-            query = 'RELEASE SAVEPOINT {};'.format(self._id)
-        else:
-            query = 'COMMIT;'
-
-        try:
-            await self._connection.execute_script(query)
-        except:
-            self._state = TransactionState.FAILED
-            raise
-        else:
-            self._state = TransactionState.COMMITTED
-
-    async def rollback(self):
-        if self._connection._top_xact is self:
-            self._connection._top_xact = None
-
-        if self._state is not TransactionState.STARTED:
-            raise FatalError('cannot rollback transaction: inconsistent state')
-
-        if self._nested:
-            query = 'ROLLBACK TO {};'.format(self._id)
-        else:
-            query = 'ROLLBACK;'
-
-        try:
-            await self._connection.execute_script(query)
-        except:
-            self._state = TransactionState.FAILED
-            raise
-        else:
-            self._state = TransactionState.ROLLEDBACK
 
 
 async def connect(iri=None, *,
---
+++ asyncpg/transaction.py
@@ -0,0 +1,133 @@
+import enum
+
+from . import exceptions as apg_errors
+
+
+class TransactionState(enum.Enum):
+    NEW = 0
+    STARTED = 1
+    COMMITTED = 2
+    ROLLEDBACK = 3
+    FAILED = 4
+
+
+class Transaction:
+
+    ISOLATION_LEVELS = {'read_committed', 'serializable', 'repeatable_read'}
+
+    __slots__ = ('_connection', '_isolation', '_readonly', '_deferrable',
+                 '_state', '_nested', '_id')
+
+    def __init__(self, connection, isolation, readonly, deferrable):
+        if isolation not in self.ISOLATION_LEVELS:
+            raise ValueError(
+                'isolation is expected to be either of {}, '
+                'got {!r}'.format(self.ISOLATION_LEVELS, isolation))
+
+        if isolation != 'serializable':
+            if readonly:
+                raise ValueError(
+                    '"readonly" is only supported for '
+                    'serializable transactions')
+
+            if deferrable and not readonly:
+                raise ValueError(
+                    '"deferrable" is only supported for '
+                    'serializable readonly transactions')
+
+        self._connection = connection
+        self._isolation = isolation
+        self._readonly = readonly
+        self._deferrable = deferrable
+        self._state = TransactionState.NEW
+        self._nested = False
+        self._id = None
+
+    async def __aenter__(self):
+        await self.start()
+
+    async def __aexit__(self, extype, ex, tb):
+        if extype is not None:
+            await self.rollback()
+
+    async def start(self):
+        if self._state is not TransactionState.NEW:
+            raise apg_errors.FatalError(
+                'cannot start transaction: inconsistent state')
+
+        con = self._connection
+
+        if con._top_xact is None:
+            con._top_xact = self
+        else:
+            # Nested transaction block
+            top_xact = con._top_xact
+            if self._isolation != top_xact._isolation:
+                raise apg_errors.FatalError(
+                    'nested transaction has different isolation level: '
+                    'current {!r} != outer {!r}'.format(
+                        self._isolation, top_xact._isolation))
+            self._nested = True
+
+        if self._nested:
+            self._id = con._get_unique_id()
+            query = 'SAVEPOINT {};'.format(self._id)
+        else:
+            if self._isolation == 'read_committed':
+                query = 'BEGIN;'
+            elif self._isolation == 'repeatable_read':
+                query = 'BEGIN ISOLATION LEVEL REPEATABLE READ;'
+            else:
+                query = 'BEGIN ISOLATION LEVEL SERIALIZABLE'
+                if self._readonly:
+                    query += ' READ ONLY'
+                if self._deferrable:
+                    query += ' DEFERRABLE'
+                query += ';'
+
+        try:
+            await self._connection.execute_script(query)
+        except:
+            self._state = TransactionState.FAILED
+            raise
+        else:
+            self._state = TransactionState.STARTED
+
+    async def commit(self):
+        if self._state is not TransactionState.STARTED:
+            raise apg_errors.FatalError(
+                'cannot commit transaction: inconsistent state')
+
+        if self._nested:
+            query = 'RELEASE SAVEPOINT {};'.format(self._id)
+        else:
+            query = 'COMMIT;'
+
+        try:
+            await self._connection.execute_script(query)
+        except:
+            self._state = TransactionState.FAILED
+            raise
+        else:
+            self._state = TransactionState.COMMITTED
+
+    async def rollback(self):
+        if self._connection._top_xact is self:
+            self._connection._top_xact = None
+
+        if self._state is not TransactionState.STARTED:
+            raise apg_errors.FatalError(
+                'cannot rollback transaction: inconsistent state')
+
+        if self._nested:
+            query = 'ROLLBACK TO {};'.format(self._id)
+        else:
+            query = 'ROLLBACK;'
+
+        try:
+            await self._connection.execute_script(query)
+        except:
+            self._state = TransactionState.FAILED
+            raise
+        else:
+            self._state = TransactionState.ROLLEDBACK
