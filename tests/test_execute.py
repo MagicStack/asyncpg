710a2a6d9c462041bae6c41f0578d99262c6a861
Yury Selivanov <yury@magic.io>
2016-06-30 20:43:45 -0400

    Test that con.execute() propagate Postgres exceptions

--- tests/test_execute.py
+++ tests/test_execute.py
@@ -25,3 +25,9 @@ class TestExecuteScript(tb.ConnectedTestCase):
             await self.con.prepare('''
                 SELECT * FROM mytab
             ''')
+
+    async def test_execute_exceptions_1(self):
+        with self.assertRaisesRegex(asyncpg.Error,
+                                    'relation "__dne__" does not exist'):
+
+            await self.con.execute('select * from __dne__')
