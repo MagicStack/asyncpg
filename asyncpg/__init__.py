ccfd5fee17c43915104bc18b436c38c30004eb47
Yury Selivanov <yury@magic.io>
2016-07-07 17:31:23 -0400

    connect: Add DSN parsing and tests

--- asyncpg/__init__.py
+++ asyncpg/__init__.py
@@ -1,6 +1,7 @@
 import asyncio
 import getpass
 import os
+import urllib.parse
 
 from .exceptions import *
 from . import connection
@@ -10,45 +11,24 @@ from . import protocol
 __all__ = ('connect',) + exceptions.__all__
 
 
-async def connect(iri=None, *,
+async def connect(dsn=None, *,
                   host=None, port=None,
                   user=None, password=None,
                   database=None,
                   loop=None,
-                  timeout=60):
+                  timeout=60,
+                  **kwargs):
 
     if loop is None:
         loop = asyncio.get_event_loop()
 
-    # On env-var -> connection parameter conversion read here:
-    # https://www.postgresql.org/docs/current/static/libpq-envars.html
-    # Note that env values may be an empty string in cases when
-    # the variable is "unset" by setting it to an empty value
-    #
-    if host is None:
-        host = os.getenv('PGHOST')
-        if not host:
-            host = ['/tmp', '/private/tmp',
-                    '/var/pgsql_socket', '/run/postgresql',
-                    'localhost']
-    if not isinstance(host, list):
-        host = [host]
+    host, port, user, password, database, kwargs = _parse_connect_params(
+        dsn=dsn, host=host, port=port, user=user, password=password,
+        database=database, kwargs=kwargs)
 
-    if port is None:
-        port = os.getenv('PGPORT')
-        if not port:
-            port = 5432
-
-    if user is None:
-        user = os.getenv('PGUSER')
-        if not user:
-            user = getpass.getuser()
-
-    if password is None:
-        password = os.getenv('PGPASSWORD')
-
-    if database is None:
-        database = os.getenv('PGDATABASE')
+    if kwargs:
+        raise RuntimeError(
+            'arbitrary connection arguments are not yet supported')
 
     last_ex = None
     for h in host:
@@ -83,6 +63,110 @@ async def connect(iri=None, *,
         raise
 
     return connection.Connection(pr, tr, loop)
+
+
+def _parse_connect_params(*, dsn, host, port, user,
+                          password, database, kwargs):
+
+    if dsn:
+        parsed = urllib.parse.urlparse(dsn)
+
+        if parsed.scheme not in {'postgresql', 'postgres'}:
+            raise ValueError(
+                'invalid DSN: scheme is expected to be either of '
+                '"postgresql" or "postgres", got {!r}'.format(parsed.scheme))
+
+        if parsed.port and port is None:
+            port = int(parsed.port)
+
+        if parsed.hostname and host is None:
+            host = parsed.hostname
+
+        if parsed.path and database is None:
+            database = parsed.path
+            if database.startswith('/'):
+                database = database[1:]
+
+        if parsed.username and user is None:
+            user = parsed.username
+
+        if parsed.password and password is None:
+            password = parsed.password
+
+        if parsed.query:
+            query = urllib.parse.parse_qs(parsed.query, strict_parsing=True)
+            for key, val in query.items():
+                if isinstance(val, list):
+                    query[key] = val[-1]
+
+            if 'host' in query:
+                val = query.pop('host')
+                if host is None:
+                    host = val
+
+            if 'port' in query:
+                val = int(query.pop('port'))
+                if port is None:
+                    port = val
+
+            if 'dbname' in query:
+                val = query.pop('dbname')
+                if database is None:
+                    database = val
+
+            if 'database' in query:
+                val = query.pop('database')
+                if database is None:
+                    database = val
+
+            if 'user' in query:
+                val = query.pop('user')
+                if user is None:
+                    user = val
+
+            if 'password' in query:
+                val = query.pop('password')
+                if password is None:
+                    password = val
+
+            if query:
+                kwargs = {**query, **kwargs}
+
+    # On env-var -> connection parameter conversion read here:
+    # https://www.postgresql.org/docs/current/static/libpq-envars.html
+    # Note that env values may be an empty string in cases when
+    # the variable is "unset" by setting it to an empty value
+    #
+    if host is None:
+        host = os.getenv('PGHOST')
+        if not host:
+            host = ['/tmp', '/private/tmp',
+                    '/var/pgsql_socket', '/run/postgresql',
+                    'localhost']
+    if not isinstance(host, list):
+        host = [host]
+
+    if port is None:
+        port = os.getenv('PGPORT')
+        if port:
+            port = int(port)
+        else:
+            port = 5432
+    else:
+        port = int(port)
+
+    if user is None:
+        user = os.getenv('PGUSER')
+        if not user:
+            user = getpass.getuser()
+
+    if password is None:
+        password = os.getenv('PGPASSWORD')
+
+    if database is None:
+        database = os.getenv('PGDATABASE')
+
+    return host, port, user, password, database, kwargs
 
 
 def _create_future(loop):
--- tests/test_connect.py
+++ tests/test_connect.py
@@ -1,4 +1,7 @@
 import asyncpg
+import contextlib
+import os
+import unittest
 
 from asyncpg import _testbase as tb
 
@@ -9,3 +12,211 @@ class TestConnect(tb.ConnectedTestCase):
         with self.assertRaisesRegex(
                 Exception, 'role "__does_not_exist__" does not exist'):
             await asyncpg.connect(user="__does_not_exist__", loop=self.loop)
+
+
+class TestConnectParams(unittest.TestCase):
+
+    TESTS = [
+        {
+            'env': {
+                'PGUSER': 'user',
+                'PGDATABASE': 'testdb',
+                'PGPASSWORD': 'passw',
+                'PGHOST': 'host',
+                'PGPORT': '123'
+            },
+            'result': (['host'], 123, 'user', 'passw', 'testdb', {})
+        },
+
+        {
+            'env': {
+                'PGUSER': 'user',
+                'PGDATABASE': 'testdb',
+                'PGPASSWORD': 'passw',
+                'PGHOST': 'host',
+                'PGPORT': '123'
+            },
+
+            'host': 'host2',
+            'port': '456',
+            'user': 'user2',
+            'password': 'passw2',
+            'database': 'db2',
+
+            'result': (['host2'], 456, 'user2', 'passw2', 'db2', {})
+        },
+
+        {
+            'env': {
+                'PGUSER': 'user',
+                'PGDATABASE': 'testdb',
+                'PGPASSWORD': 'passw',
+                'PGHOST': 'host',
+                'PGPORT': '123'
+            },
+
+            'dsn': 'postgres://user3:123123@localhost/abcdef',
+
+            'host': 'host2',
+            'port': '456',
+            'user': 'user2',
+            'password': 'passw2',
+            'database': 'db2',
+
+            'result': (['host2'], 456, 'user2', 'passw2', 'db2', {})
+        },
+
+        {
+            'env': {
+                'PGUSER': 'user',
+                'PGDATABASE': 'testdb',
+                'PGPASSWORD': 'passw',
+                'PGHOST': 'host',
+                'PGPORT': '123'
+            },
+
+            'dsn': 'postgres://user3:123123@localhost:5555/abcdef',
+
+            'result': (['localhost'], 5555, 'user3', '123123', 'abcdef', {})
+        },
+
+        {
+            'dsn': 'postgres://user3:123123@localhost:5555/abcdef',
+            'result': (['localhost'], 5555, 'user3', '123123', 'abcdef', {})
+        },
+
+        {
+            'dsn': 'postgresql://user3:123123@localhost:5555/'
+                   'abcdef?param=sss&param=123&host=testhost&user=testuser'
+                   '&port=2222&database=testdb',
+            'host': '127.0.0.1',
+            'port': '888',
+            'user': 'me',
+            'password': 'ask',
+            'database': 'db',
+            'result': (['127.0.0.1'], 888, 'me', 'ask', 'db', {'param': '123'})
+        },
+
+        {
+            'dsn': 'postgresql:///dbname?host=/unix_sock/test&user=spam',
+            'result': (['/unix_sock/test'], 5432, 'spam', None, 'dbname', {})
+        },
+
+        {
+            'dsn': 'pq:///dbname?host=/unix_sock/test&user=spam',
+            'error': (ValueError, 'invalid DSN')
+        },
+    ]
+
+    @contextlib.contextmanager
+    def environ(self, **kwargs):
+        old_vals = {}
+        for key in kwargs:
+            if key in os.environ:
+                old_vals[key] = os.environ[key]
+
+        for key, val in kwargs.items():
+            if val is None:
+                if key in os.environ:
+                    del os.environ[key]
+            else:
+                os.environ[key] = val
+
+        try:
+            yield
+        finally:
+            for key in kwargs:
+                if key in os.environ:
+                    del os.environ[key]
+            for key, val in old_vals.items():
+                os.environ[key] = val
+
+    def run_testcase(self, testcase):
+        env = testcase.get('env', {})
+        test_env = {'PGHOST': None, 'PGPORT': None,
+                    'PGUSER': None, 'PGPASSWORD': None,
+                    'PGDATABASE': None}
+        test_env.update(env)
+
+        dsn = testcase.get('dsn')
+        kwargs = testcase.get('kwargs', {})
+        user = testcase.get('user')
+        port = testcase.get('port')
+        host = testcase.get('host')
+        password = testcase.get('password')
+        database = testcase.get('database')
+
+        expected = testcase.get('result')
+        expected_error = testcase.get('error')
+        if expected is None and expected_error is None:
+            raise RuntimeError(
+                'invalid test case: either "result" or "error" key '
+                'has to be specified')
+        if expected is not None and expected_error is not None:
+            raise RuntimeError(
+                'invalid test case: either "result" or "error" key '
+                'has to be specified, got both')
+
+        with contextlib.ExitStack() as es:
+            es.enter_context(self.subTest(dsn=dsn, kwargs=kwargs, env=env))
+            es.enter_context(self.environ(**test_env))
+
+            if expected_error:
+                es.enter_context(self.assertRaisesRegex(*expected_error))
+
+            result = asyncpg._parse_connect_params(
+                dsn=dsn, host=host, port=port, user=user, password=password,
+                database=database, kwargs=kwargs)
+
+        if expected is not None:
+            self.assertEqual(expected, result)
+
+    def test_test_connect_params_environ(self):
+        self.assertNotIn('AAAAAAAAAA123', os.environ)
+        self.assertNotIn('AAAAAAAAAA456', os.environ)
+        self.assertNotIn('AAAAAAAAAA789', os.environ)
+
+        try:
+
+            os.environ['AAAAAAAAAA456'] = '123'
+            os.environ['AAAAAAAAAA789'] = '123'
+
+            with self.environ(AAAAAAAAAA123='1',
+                              AAAAAAAAAA456='2',
+                              AAAAAAAAAA789=None):
+
+                self.assertEqual(os.environ['AAAAAAAAAA123'], '1')
+                self.assertEqual(os.environ['AAAAAAAAAA456'], '2')
+                self.assertNotIn('AAAAAAAAAA789', os.environ)
+
+            self.assertNotIn('AAAAAAAAAA123', os.environ)
+            self.assertEqual(os.environ['AAAAAAAAAA456'], '123')
+            self.assertEqual(os.environ['AAAAAAAAAA789'], '123')
+
+        finally:
+            for key in {'AAAAAAAAAA123', 'AAAAAAAAAA456', 'AAAAAAAAAA789'}:
+                if key in os.environ:
+                    del os.environ[key]
+
+    def test_test_connect_params_run_testcase(self):
+        with self.environ(PGPORT='777'):
+            self.run_testcase({
+                'env': {
+                    'PGUSER': '__test__'
+                },
+                'host': 'abc',
+                'result': (['abc'], 5432, '__test__', None, None, {})
+            })
+
+        with self.assertRaises(AssertionError):
+            self.run_testcase({
+                'env': {
+                    'PGUSER': '__test__'
+                },
+                'host': 'abc',
+                'result': (['abc'], 5432, 'wrong_user', None, None, {})
+            })
+
+    def test_connect_params(self):
+        for testcase in self.TESTS:
+            self.run_testcase(testcase)
