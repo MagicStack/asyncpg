976d8897baac2af2d1cc99c7a9b80273c920ec96
Elvis Pranskevichus <elvis@magic.io>
2016-07-16 11:12:33 -0400

    Require Python 3.5 or greater

--- setup.py
+++ setup.py
@@ -5,17 +5,24 @@
 # the Apache 2.0 License: http://www.apache.org/licenses/LICENSE-2.0
 
 
-from setuptools import setup, Extension
+import setuptools
+import sys
 
 
-setup(
+if sys.version_info < (3, 5):
+    raise RuntimeError('asyncpg requires Python 3.5 or greater')
+
+
+setuptools.setup(
     name='asyncpg',
     version='0.0.1',
     description='An asyncio PosgtreSQL driver',
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Operating System :: POSIX',
         'Operating System :: MacOS :: MacOS X',
         'Development Status :: 4 - Beta',
@@ -28,9 +35,10 @@ setup(
     provides=['asyncpg'],
     include_package_data=True,
     ext_modules=[
-        Extension("asyncpg.protocol.protocol",
-                  ["asyncpg/protocol/record/recordobj.c",
-                   "asyncpg/protocol/protocol.c"],
-                  extra_compile_args=['-O2'])
+        setuptools.Extension(
+            "asyncpg.protocol.protocol",
+            ["asyncpg/protocol/record/recordobj.c",
+             "asyncpg/protocol/protocol.c"],
+            extra_compile_args=['-O2'])
     ]
 )
