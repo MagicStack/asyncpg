9dc1c16bd7886f3443fa02bd08991685cb776a9f
Yury Selivanov <yury@magic.io>
2016-07-17 11:08:18 -0400

    Add license headers to pool.py and test_pool.py

--- asyncpg/pool.py
+++ asyncpg/pool.py
@@ -1,3 +1,10 @@
+# Copyright (C) 2016-present the ayncpg authors and contributors
+# <see AUTHORS file>
+#
+# This module is part of asyncpg and is released under
+# the Apache 2.0 License: http://www.apache.org/licenses/LICENSE-2.0
+
+
 import asyncio
 
 from . import connection
