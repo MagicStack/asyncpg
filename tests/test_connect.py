dc5f6fc5cf86b3ed0c5fb895fc9df06939aea733
Elvis Pranskevichus <elvis@magic.io>
2016-07-28 18:02:10 -0400

    Drop redundant connection test
    This case is already covered by the authentication test suite

--- tests/test_connect.py
+++ tests/test_connect.py
@@ -15,13 +15,6 @@ from asyncpg import _testbase as tb
 from asyncpg.connection import _parse_connect_params
 
 
-class TestConnect(tb.ConnectedTestCase):
-    async def test_connect_1(self):
-        with self.assertRaisesRegex(
-                Exception, 'role "__does_not_exist__" does not exist'):
-            await asyncpg.connect(user="__does_not_exist__", loop=self.loop)
-
-
 class TestSettings(tb.ConnectedTestCase):
 
     async def test_get_settings_01(self):
