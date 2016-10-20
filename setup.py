910ed3544e9b70a2ea3f4a7940b60edddf444b45
Elvis Pranskevichus <elvis@magic.io>
2016-10-20 12:55:14 -0400

    asyncpg v0.6.1

--- setup.py
+++ setup.py
@@ -162,7 +162,7 @@ class build_ext(_build_ext.build_ext):
 
 setuptools.setup(
     name='asyncpg',
-    version='0.6.0',
+    version='0.6.1',
     description='An asyncio PosgtreSQL driver',
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
