a60bce703f2d5f5f4d511987d98419d02e0ce29c
Elvis Pranskevichus <elvis@magic.io>
2018-05-29 01:53:18 +0000

    Automatic documentation update

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,5 +1,5 @@
 var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '0.16.0.dev0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
