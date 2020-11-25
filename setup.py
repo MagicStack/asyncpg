8bc5c667f45fe06b9a7775a538b24273a174c60a
Stefan Tjarks <66305+stj@users.noreply.github.com>
2020-11-24 17:55:11 -0800

    Update CPython versions in build environments
    Add build against CPython 3.9.
    Update aarch64 build to use CPython 3.10-dev, latest supported by travis-ci.

--- setup.py
+++ setup.py
@@ -263,6 +263,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Database :: Front-Ends',
     ],
