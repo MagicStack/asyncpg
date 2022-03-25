e65618464497ac479f064f2dc4ab7fdf87dbe2fe
Elvis Pranskevichus <elvis@magic.io>
2022-03-25 15:48:03 -0700

    Update tests/test_introspection.py

--- tests/test_introspection.py
+++ tests/test_introspection.py
@@ -193,7 +193,7 @@ class TestIntrospection(tb.ConnectedTestCase):
 
     @tb.with_connection_options(database='asyncpg_intro_test')
     async def test_introspection_loads_basetypes_of_domains(self):
-        # Test that basetypes of domains are loaded to the 
+        # Test that basetypes of domains are loaded to the
         # client encode/decode cache
         await self.con.execute('''
             DROP TABLE IF EXISTS test;
