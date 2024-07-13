9fcddfc1c0b6e949a2c16e193d5472699b78458d
Eugene Toder <eltoder@users.noreply.github.com>
2024-07-13 06:33:14 -0400

    Use asyncio.run() instead of run_until_complete() (#1140)
    Mostly in the documentation.

--- tools/generate_type_map.py
+++ tools/generate_type_map.py
@@ -124,10 +124,7 @@ def main():
         help='PostgreSQL server user')
 
     args = parser.parse_args()
-
-    loop = asyncio.get_event_loop()
-
-    loop.run_until_complete(runner(args))
+    asyncio.run(runner(args))
 
 
 if __name__ == '__main__':
