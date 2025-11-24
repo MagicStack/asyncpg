648b35f18199ebf2a30009376a6e9060cf7ad789
Elvis Pranskevichus <elvis@edgedb.com>
2025-11-24 11:07:59 -0800

    Bump Cython to 3.2.1 (#1288)
    Should fix refleak issues

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools.command import sdist as setuptools_sdist
 from setuptools.command import build_ext as setuptools_build_ext
 
 
-CYTHON_DEPENDENCY = 'Cython(>=3.1.0,<4.0.0)'
+CYTHON_DEPENDENCY = 'Cython(>=3.2.1,<4.0.0)'
 
 CFLAGS = ['-O2']
 LDFLAGS = []
