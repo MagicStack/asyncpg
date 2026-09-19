6695db76c36f903044bccf8d2d216540c74a7a24
Elvis Pranskevichus <el@prans.net>
2026-09-18 22:35:38 -0700

    Drop use of pkg_resources from setup.py (#1362)
    Use `packaging` as a modern alternative.

--- setup.py
+++ setup.py
@@ -17,6 +17,8 @@ import platform
 import re
 import subprocess
 
+from packaging.requirements import Requirement
+
 # We use vanilla build_ext, to avoid importing Cython via
 # the setuptools version.
 import setuptools
@@ -188,8 +190,6 @@ class build_ext(setuptools_build_ext.build_ext):
                         need_cythonize = True
 
         if need_cythonize:
-            import pkg_resources
-
             # Double check Cython presence in case setup_requires
             # didn't go into effect (most likely because someone
             # imported Cython before setup_requires injected the
@@ -201,8 +201,8 @@ class build_ext(setuptools_build_ext.build_ext):
                     'please install {} to compile asyncpg from source'.format(
                         CYTHON_DEPENDENCY))
 
-            cython_dep = pkg_resources.Requirement.parse(CYTHON_DEPENDENCY)
-            if Cython.__version__ not in cython_dep:
+            cython_dep = Requirement(CYTHON_DEPENDENCY)
+            if Cython.__version__ not in cython_dep.specifier:
                 raise RuntimeError(
                     'asyncpg requires {}, got Cython=={}'.format(
                         CYTHON_DEPENDENCY, Cython.__version__
